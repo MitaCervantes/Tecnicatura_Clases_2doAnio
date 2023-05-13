350d9a7715774f7d07d62f591271d94ac616252e
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2023-05-13 16:50:33 -0300

    5ta clase Python - Registros con Postgresql y Python

--- Python/Leccion4/BD/prueba_bd.py
+++ Python/Leccion4/BD/prueba_bd.py
@@ -7,10 +7,21 @@ conexion = psycopg2.connect(
     port='5432',
     database='test_bd'
 )
-cursor = conexion.cursor()
-sentencia = 'SELECT * FROM persona'
-cursor.execute(sentencia) # De esta manera ejecutamos la sentencia
-registros = cursor.fetchall() # Recuperamos todos los registros que seran una lista
-print(registros)
-cursor.close()
-conexion.close()
+try:
+    with conexion:
+        with conexion.cursor() as cursor:
+            #cursor = conexion.cursor()
+            sentencia = 'SELECT * FROM persona WHERE id_persona = %s' #Placeholder
+            id_persona = input("Digite un número para el id_persona: ")
+            cursor.execute(sentencia, (id_persona,)) # De esta manera ejecutamos la sentencia
+            #registros = cursor.fetchall() # Recuperamos todos los registros que seran una lista
+            registros = cursor.fetchone() #No trae un array sino un tupla
+            print(registros)
+except Exception as e:
+    print(f'Ocurrio un error {e}')
+finally:
+    #cursor.close()
+    conexion.close()
+
+#https://www.psycopg.org/docs/usage.html
+
