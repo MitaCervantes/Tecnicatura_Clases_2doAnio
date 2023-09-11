b1d2d6cdecc00047cab8e735e554485fca4be011
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2023-09-11 18:37:38 -0300

    Clase 03 JavaScript - E-commerce Básico parte 3

--- Proyectos JavaScript/E-COMMERCE-2022/client/js/products.js
+++ Proyectos JavaScript/E-COMMERCE-2022/client/js/products.js
@@ -3,42 +3,42 @@ const products = [
       "id": 1,
       "productName": "Banana",
       "price": 480,
-      "quantity": 1,
+      "quanty": 1,
       "img": ""
     },
     {
       "id": 2,
       "productName": "Leche",
       "price": 950,
-      "quantity": 1,
+      "quanty": 1,
       "img": ""
     },
     {
       "id": 3,
       "productName": "Pollo",
       "price": 750,
-      "quantity": 1,
+      "quanty": 1,
       "img": ""
     },
     {
       "id": 4,
       "productName": "Enlatados",
       "price": 250,
-      "quantity": 1,
+      "quanty": 1,
       "img": ""
     },
     {
       "id": 5,
       "productName": "Mayonesa",
       "price": 550,
-      "quantity": 1,
+      "quanty": 1,
       "img": ""
     },
     {
       "id": 6,
       "productName": "Varios",
       "price": 480,
-      "quantity": 1,
+      "quanty": 1,
       "img": ""
     }
   ];
