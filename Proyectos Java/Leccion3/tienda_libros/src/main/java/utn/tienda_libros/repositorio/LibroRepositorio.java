30fca675a165952fb32cc53b464e2c999f160992
Mariana_Cervantes <cervantes.mariana.6017@gmail.com>
2023-10-11 18:10:28 -0300

    Clase 08 Java - Tienda Libros SpringBoot y Swing - Parte 03

--- Proyectos Java/Leccion3/tienda_libros/src/main/java/utn/tienda_libros/repositorio/LibroRepositorio.java
+++ Proyectos Java/Leccion3/tienda_libros/src/main/java/utn/tienda_libros/repositorio/LibroRepositorio.java
@@ -4,4 +4,5 @@ import utn.tienda_libros.modelo.Libro;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface LibroRepositorio extends JpaRepository<Libro, Integer> {
+
 }
